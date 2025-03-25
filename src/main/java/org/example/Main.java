072edc69656502385198acdaa96d3d9171aae78f
edusantosfr <70604190+edusantosfr@users.noreply.github.com>
2025-03-25 11:39:58 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -143,9 +143,9 @@ public class Main {
         for (int i = 0; i < secretNumber.length; i++) {
             secretNumber[i] = random.nextInt(1, difficulty);
         }
-        for (int j : secretNumber) {
-            System.out.println(j);
-        }
+        //for (int j : secretNumber) {
+        //    System.out.println(j);
+        //}
 
         int guessedNumber;
         int pointsGuesses = 0;
